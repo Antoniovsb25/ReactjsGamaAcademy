0a73ea2fb1588d74a913c30e8b17bfdd94e25b6b
Antônio Víctor Souza Basílio <antoniovsb25@gmail.com>
2021-07-15 23:48:35 -0300

    Commit da aula 04 - consumindo dados de uma API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App(props) {
-  
+
+  const [usuario, setUsuario] = React.useState('')
+
+  function handlePesquisa() {
+    console.log(usuario)
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then((response) => {
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
     <>
-    <input className="usuarioInput" placeholder="usernameGithub" />
-    <button type="button">Pesquisar</button>
+    <input className="usuarioInput" placeholder="usernameGithub" value={usuario} onChange={(e) => setUsuario(e.target.value)}/>
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
